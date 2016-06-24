7f072e6a29c654767a373692767e014b85e5e417
Andrew Cookson <acookson91@gmail.com>
2016-06-24 16:00:12 +0100

    Test passings as were now showing all languages on repo page

--- nasa-lang-finder/app/controllers/repos_controller.rb
+++ nasa-lang-finder/app/controllers/repos_controller.rb
@@ -1,6 +1,12 @@
 class ReposController < ApplicationController
 
   def index
+    response = HTTParty.get('https://api.github.com/repos/nasa/' + params[:repo_name] + '/languages?client_id=8e095b5598d3114163bb&client_secret=6e9182c238ef8dd639440302bacc7eb049c38d9e')
+    @repo_data =  get_repo_data(response.parsed_response)
   end
-  
+
+  def get_repo_data(response)
+    [ {repo_name: params[:repo_name] ,languages: response }]
+  end
+
 end
--- nasa-lang-finder/spec/controllers/repos_controller_spec.rb
+++ nasa-lang-finder/spec/controllers/repos_controller_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe ReposController, type: :controller do
 
+  # before do
+  #   controller.stub(:params).and_return({:repo_name => "38A"})
+  # end
+  #
+  # it 'gets languages from url' do
+  #   lang_response = {"JavaScript": 100695,"CSS": 20492,"Ruby": 5127,"HTML": 4255}
+  #   expect(subject.get_repo_data(lang_response)).to eq({repo_name: '38A' ,languages: lang_response })
+  # end
+
 end
