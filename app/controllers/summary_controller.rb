e571fb1592ffc843b0155ee752d66eadf04f1b4c
Andrew Cookson <acookson91@gmail.com>
2016-06-26 20:10:53 +0100

    added passing test, flash message show if connect made

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -4,7 +4,12 @@ class SummaryController < ApplicationController
 
   def index
     response = HTTParty.get('https://api.github.com/users/nasa/repos?client_id=8e095b5598d3114163bb&client_secret=6e9182c238ef8dd639440302bacc7eb049c38d9e')
-    @repos_info = response.map{|repo| {name:repo['name'], description: repo['description']} }
+    if response.empty?
+      flash.now[:danger] = 'NASA Repo returned nothing'
+    else
+      flash.now[:success] = 'Successfully connected to NASA Github'
+      @repos_info = response.map{|repo| {name:repo['name'], description: repo['description']} }
+    end
   end
-
+  
 end
