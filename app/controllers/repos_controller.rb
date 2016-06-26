9a80cf07f9bc48d3081ef5f6cc3a7d928546df3b
Andrew Cookson <acookson91@gmail.com>
2016-06-26 20:17:11 +0100

    added evn gem

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,7 +1,7 @@
 class ReposController < ApplicationController
 
   def index
-    response = HTTParty.get('https://api.github.com/repos/nasa/' + params[:repo_name] + '/languages?client_id=8e095b5598d3114163bb&client_secret=6e9182c238ef8dd639440302bacc7eb049c38d9e')
+    response = HTTParty.get('https://api.github.com/repos/nasa/' + params[:repo_name] + '/languages?client_id=' + ENV['CLIENT_ID'] + '&client_secret=' + ENV['CLIENT_SECRET'])
     if response.parsed_response.empty?
       @repo_data =  get_repo_data(response.parsed_response)
       flash.now[:danger] = 'This Nasa Repo contains no code'
--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -3,13 +3,13 @@ require 'httparty'
 class SummaryController < ApplicationController
 
   def index
-    response = HTTParty.get('https://api.github.com/users/nasa/repos?client_id=8e095b5598d3114163bb&client_secret=6e9182c238ef8dd639440302bacc7eb049c38d9e')
+    response = HTTParty.get('https://api.github.com/users/nasa/repos?client_id=' + ENV['CLIENT_ID'] + '&client_secret=' + ENV['CLIENT_SECRET'])
     if response.empty?
-      flash.now[:danger] = 'NASA Repo returned nothing'
+      flash.now[:danger] = 'NASA Github returned nothing'
     else
       flash.now[:success] = 'Successfully connected to NASA Github'
       @repos_info = response.map{|repo| {name:repo['name'], description: repo['description']} }
     end
   end
-  
+
 end
